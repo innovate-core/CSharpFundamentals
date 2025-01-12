9dfa1c5d16eba956fe10303efaab53e89e242bab
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2025-01-12 21:44:37 +0200

    Type Conversion

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -5,5 +5,6 @@ internal class Program
     static void Main(string[] args)
     {
         VariablesAndConstants.VariablesAndConstantsProgram();
+        TypeConversion.TypeConversionProgram();
     }
 }
---
+++ CSharpFundamentals/TypeConversion.cs
@@ -0,0 +1,45 @@
+﻿namespace CSharpFundamentals;
+
+public class TypeConversion
+{
+    public static void TypeConversionProgram()
+    {
+        Console.WriteLine("Start -> Type Conversion");
+
+        //byte b = 1;
+        //int i = b;
+        //Console.WriteLine(i);
+
+        //int i = 1000;
+        //byte b = (byte)i;
+        //Console.WriteLine(b);
+
+        //var number = "1234";
+        //int i = Convert.ToInt32(number);
+        //Console.WriteLine(i);
+
+        //try
+        //{
+        //    var number = "1234";
+        //    byte b = Convert.ToByte(number);
+        //    Console.WriteLine(b);
+        //}
+        //catch (Exception)
+        //{
+        //    Console.WriteLine("The number could not be converted to a byte.");
+        //}
+
+        try
+        {
+            var str = "true";
+            bool b  = Convert.ToBoolean(str);
+            Console.WriteLine(b);
+        }
+        catch (Exception)
+        {
+        }
+
+
+        Console.WriteLine("Finish -> Type Conversion");
+    }
+}
