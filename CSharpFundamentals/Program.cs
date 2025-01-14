e7b5f8f5a52f0fe3aeca3c0c9147ad598211a65d
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2025-01-14 22:37:59 +0200

    If/Else and Switch/Case

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -4,14 +4,19 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        //Primitive Types and Expressions
         VariablesAndConstants.VariablesAndConstantsProgram();
         TypeConversion.TypeConversionProgram();
         Operators.OperatorsProgram();
 
+        //Non-Primitive Types
         Classes.ClassProgram();
         Arrays.ArraysProgram();
         Strings.StringsProgram();
         Enums.EnumsProgram();
         ReferenceTypesAndValueTypes.ReferenceTypesAndValueTypesProgram();
+
+        //Control Flow
+        IfElseAndSwitchCase.IfElseAndSwitchCaseProgram();
     }
 }
