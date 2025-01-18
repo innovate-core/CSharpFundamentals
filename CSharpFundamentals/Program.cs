baa502a7066752aa82409cc0b03253a024816853
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2025-01-18 20:29:58 +0200

    Exercises

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -4,23 +4,25 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        //Primitive Types and Expressions
-        VariablesAndConstants.VariablesAndConstantsProgram();
-        TypeConversion.TypeConversionProgram();
-        Operators.OperatorsProgram();
+        ////Primitive Types and Expressions
+        //VariablesAndConstants.VariablesAndConstantsProgram();
+        //TypeConversion.TypeConversionProgram();
+        //Operators.OperatorsProgram();
 
-        //Non-Primitive Types
-        Classes.ClassProgram();
-        Arrays.ArraysProgram();
-        Strings.StringsProgram();
-        Enums.EnumsProgram();
-        ReferenceTypesAndValueTypes.ReferenceTypesAndValueTypesProgram();
+        ////Non-Primitive Types
+        //Classes.ClassProgram();
+        //Arrays.ArraysProgram();
+        //Strings.StringsProgram();
+        //Enums.EnumsProgram();
+        //ReferenceTypesAndValueTypes.ReferenceTypesAndValueTypesProgram();
 
-        //Control Flow
-        IfElseAndSwitchCase.IfElseAndSwitchCaseProgram();
-        ForLoops.ForLoopsProgram();
-        ForeachLoops.ForeachLoopsProgram();
-        WhileLoops.WhileLoopsProgram();
-        RandomClass.RandomClassProgram();
+        ////Control Flow
+        //IfElseAndSwitchCase.IfElseAndSwitchCaseProgram();
+        //ForLoops.ForLoopsProgram();
+        //ForeachLoops.ForeachLoopsProgram();
+        //WhileLoops.WhileLoopsProgram();
+        //RandomClass.RandomClassProgram();
+
+        Exercises.ExercisesProgram();
     }
 }
